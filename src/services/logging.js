f616e173064debd023d0641e40585e8aee8effcb
Visahavel <wizvishak@gmail.com>
2020-06-06 16:12:28 +0530

    [ADD] log nazar service activity

--- src/services/logging.js
+++ src/services/logging.js
@@ -1,10 +1,10 @@
 import Service from '../lib/structures/service';
 import Logger from '../lib/plugins/winston';
 
-
 export default class extends Service {
 	constructor(...args) {
 		super(...args);
+
 		this.LOGGER = Logger;
 	}
 
